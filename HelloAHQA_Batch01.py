3823b52752ade9a824882533163dfe28110d1b08
geurh <brayn.heartnet@gmail.com>
2021-07-30 16:54:03 +0700

    - modified HelloAHQA_Batch01.py my print was missing, somebody accidentally delete it

--- HelloAHQA_Batch01.py
+++ HelloAHQA_Batch01.py
@@ -6,7 +6,7 @@ print(hello_pre + flo + hello_post)
 # Write your code to print your name below this line!
 # For example "Hello, Johny Depp!"
 hasan = "Muhammad Hasan Muntaha"
-
+print(hello_pre + hasan + hello_post)
 
 
 
