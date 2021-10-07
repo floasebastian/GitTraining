61a9977e6cff94dd615975c31018e08d33df4e1c
Dani Adityatama <dani.adityatama@gmail.com>
2021-10-07 17:08:41 +0700

    myname = "Dani Adityatama" print(hello_pre + "my name is " + myname + hello_post)

--- Batch_002/Hello_Batch_002.py
+++ Batch_002/Hello_Batch_002.py
@@ -5,3 +5,6 @@ print(hello_pre + flo + hello_post)
 
 # Write your code to print your name below this line!
 # For example "Hello, Johny Depp!"
+
+myname = "Dani Adityatama"
+print(hello_pre + "my name is " + myname + hello_post)
