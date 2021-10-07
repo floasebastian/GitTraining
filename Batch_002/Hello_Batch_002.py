0253f6922e37f3d2f85b544fc5e2ba773ae6d606
Ariella "YezerieL" Cristian <cristianariella@gmail.com>
2021-10-07 18:17:16 +0700

    added Ariella Putra

--- Batch_002/Hello_Batch_002.py
+++ Batch_002/Hello_Batch_002.py
@@ -6,13 +6,14 @@ print(hello_pre + flo + hello_post)
 # Write your code to print your name below this line!
 # For example "Hello, Johny Depp!"
 
-=======
 myname = "Sigit Nugraha"
 print("Hello, " + myname + "!")
 
-=======
 myname = "Dani Adityatama"
 print(hello_pre + "my name is " + myname + hello_post)
 
 rasu = "Ragil Sutresno"
 print(hello_pre + rasu + hello_post)
+
+yezeriel = 'Ariella Putra'
+print(f'{hello_pre}{yezeriel}{hello_post}')
