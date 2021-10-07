c070182fb7a4d85495741e10c72c073a2b1db856
Flo Sebastian <flo.sebastian@gameloft.com>
2021-10-07 15:55:42 +0700

    + Added new file to Batch_002 folder

---
+++ Batch_002/Hello_Batch_002.py
@@ -0,0 +1,7 @@
+hello_pre = "Hello, "
+hello_post = "!\n"
+flo = "Flo Sebastian"
+print(hello_pre + flo + hello_post)
+
+# Write your code to print your name below this line!
+# For example "Hello, Johny Depp!"
